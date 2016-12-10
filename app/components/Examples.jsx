f939df3d882d1c5a4310b91f42c1cffd252e002d
=Fred Gravil <fgravil18@knights.ucf.edu>
2016-12-10 15:03:05 -0500

    Renamed to Examples

---
+++ app/components/Examples.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Example = (props) => {
+  return (
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to the examples page</p>
+    </div>
+  );
+}
+
+module.exports = Example;
