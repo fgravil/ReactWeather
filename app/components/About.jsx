8a461a2370c9680dafebda613a9324dac4b7d9d5
=Fred Gravil <fgravil18@knights.ucf.edu>
2016-12-18 01:38:07 -0500

    Update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,26 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application I built for learning
+        the React JS libraries. The tools I used include:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>-
+          JavaScript library used.
+        </li>
+        <li>
+          <a href="http://openweathermap.com">Open Weather Map</a>-
+          I used Open Weater Map API to search for weather data by city name.
+        </li>
+        <li>
+          <a href="http://foundation.zurb.com/">Foundation</a> -
+          Responsive front-end framework I used.
+        </li>
+      </ul>
+      <p>Fell free to download the source code
+         <a href="https://github.com/fgravil/ReactWeather"> Here</a>.</p>
     </div>
   )
 }
