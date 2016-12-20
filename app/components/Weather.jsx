8e14f45ea638604a350f1387a347ec2fd8714dfa
=Fred Gravil <fgravil18@knights.ucf.edu>
2016-12-20 08:17:41 -0500

    Add nav search support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -14,7 +14,9 @@ var Weather = React.createClass({
     var that = this;
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     OpenWeatherMap.getTemp(location).then(function(temp){
@@ -30,6 +32,22 @@ var Weather = React.createClass({
       });
       // alert(typeof e.message);
     });
+  },
+  componentDidMount: function(){
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function(newProps){
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function(){
     var {isLoading, location, temp, errorMessage} = this.state;
